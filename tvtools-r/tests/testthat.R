d53f1a2a913b339b5fc667f20f31da58987696a8
Tim Copeland <copeland.tim@gmail.com>
2025-11-22 08:24:12 +0100

    Delete tvtools-r/tests directory

--- tvtools-r/tests/testthat.R
+++
@@ -1,8 +0,0 @@
-# This file is part of the standard testthat workflow.
-# It is run by R CMD check and devtools::test().
-# It runs all tests in the tests/testthat directory.
-
-library(testthat)
-library(tvtools)
-
-test_check("tvtools")
